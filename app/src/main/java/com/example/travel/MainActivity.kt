0aa4161d841e6d91fdab66afc5acabed023aae20
Harshal Gainer <harshalgainer@gmail.com>
2023-04-19 14:49:14 +0530

    Task1 completed

--- app/src/main/java/com/example/travel/MainActivity.kt
+++ app/src/main/java/com/example/travel/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.travel
 
 import android.content.Intent
+import android.icu.text.MessagePattern.ApostropheMode
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import android.widget.Toast.LENGTH_SHORT
 
@@ -12,13 +14,22 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var button : Button = findViewById(R.id.button)
-        button.setOnClickListener(View.OnClickListener {
-            Toast.makeText( this,"You're Welcome", LENGTH_SHORT).show()
-            val intent= Intent(this@MainActivity, MainActivity3::class.java)
-            startActivity(intent)
-            finish()
-        })
+        val button = findViewById<Button>(R.id.button)
+        val har = findViewById<EditText>(R.id.har)
+        har.text = null
+        har.setOnClickListener{
+            har.text = null
+        }
+        button.setOnClickListener{
+            val ds :String = har.text.toString()
+            if(ds.isEmpty()){
+                Toast.makeText(this,"Enter your Roll No.", LENGTH_SHORT).show()
+            }
+            else{
+                val intent = Intent(this@MainActivity,MainActivity3::class.java)
+                startActivity(intent)
+            }
+        }
     }
 
 }
